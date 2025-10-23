5ed23bc824a1e9250d62fe53995b8be50fbbe798
yan-ys <songdf2@gmail.com>
2025-10-23 22:38:21 +0000

    solved problem 1 created a function that counted number of vowels in a string

---
+++ problem1.py
@@ -0,0 +1,11 @@
+def count_vowels(s):
+    #word = s
+    count = 0
+    for letter in s:
+        if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+            count += 1
+        if letter == 'A' or letter == 'E' or letter == 'I' or letter == 'O' or letter == 'U':
+            count += 1
+    return count
+
+print(count_vowels(input('type a word: ')))
